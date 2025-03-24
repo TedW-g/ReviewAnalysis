84c5d698bbecc6ae0e29d031cd1c8d3980c8a14a
TedW-g <tedw3@nycstudents.net>
2025-03-24 13:31:04 +0000

    commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        
+        Review[] coolReviews = {one(4, "Good! Thx"), two(3, "OK site", three(5, "Great!"), four(2, "Poor! Bad."), five(3, ""))};
+        System.out.println();
     }
 }
